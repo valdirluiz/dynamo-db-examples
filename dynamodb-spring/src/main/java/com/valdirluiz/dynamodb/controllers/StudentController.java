f89a7d5e423d46c468911a5244669792e66434c3
valdir <valdir@AirValdir.local>
2022-01-08 10:51:23 -0300

    Create students domain

--- dynamodb-spring/src/main/java/com/valdirluiz/dynamodb/controllers/StudentController.java
+++ dynamodb-spring/src/main/java/com/valdirluiz/dynamodb/controllers/StudentController.java
@@ -1,9 +1,14 @@
 package com.valdirluiz.dynamodb.controllers;
 
+import com.valdirluiz.dynamodb.services.StudentService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/students")
 public class StudentController {
+
+    @Autowired
+    private StudentService studentService;
 }
