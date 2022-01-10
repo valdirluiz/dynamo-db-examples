2f66cf5efe0ba77acaa1a04cc0b720add73e883d
valdir <valdir@AirValdir.local>
2022-01-10 08:23:58 -0300

    Create students domain

--- dynamodb-spring/src/main/java/com/valdirluiz/dynamodb/entitys/Student.java
+++ dynamodb-spring/src/main/java/com/valdirluiz/dynamodb/entitys/Student.java
@@ -1,7 +1,47 @@
 package com.valdirluiz.dynamodb.entitys;
 
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBAttribute;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBHashKey;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBRangeKey;
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBTable;
 
 @DynamoDBTable(tableName = "student")
 public class Student {
+
+    private Long studentId;
+    private String firstName;
+    private String lastName;
+
+    public Student(){}
+
+    public Student(Long studentId, String firstName, String lastName) {
+        this.studentId = studentId;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    @DynamoDBHashKey(attributeName = "studentId")
+    public Long getStudentId() {
+        return studentId;
+    }
+
+    public void setStudentId(Long studentId) {
+        this.studentId = studentId;
+    }
+
+    @DynamoDBRangeKey(attributeName = "firstName")
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    @DynamoDBAttribute
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) { this.lastName = lastName; }
 }
--- dynamodb-spring/src/main/java/com/valdirluiz/dynamodb/services/StudentService.java
+++ dynamodb-spring/src/main/java/com/valdirluiz/dynamodb/services/StudentService.java
@@ -1,7 +1,12 @@
 package com.valdirluiz.dynamodb.services;
 
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class StudentService {
+
+    @Autowired
+    private DynamoDBMapper mapper;
 }
